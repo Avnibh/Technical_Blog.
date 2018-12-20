9fd758339597ef9a6c098335164a58c6300fc8c3
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-20 23:30:21 +0530

    Add files via upload

---
+++ author_avni.php
@@ -0,0 +1,306 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>How To Create a Database Using myphpadmin ?</h3>
+						<p>In this article, we will see how to create a dabatase using myphpadmin panel and to run different queries in our database tables. Following are the steps to create the database :  
+                        <h4> Step 1:</h4>In the myphpadmin panel create a new database <br>
+					<br>	<figure class="pull-centre">
+							<img src="./img/BLOGS/1.png" alt="">
+						</figure>
+						<br>
+
+						<h4> Step 2:</h4>Create a table and specify its columns.
+                        </p>
+						<figure class="pull-centre">
+							<img src="./img/BLOGS/2.png" alt="">
+						</figure>
+
+						<h4> Step 3:</h4>Add the attributes and select their respective datatypes.
+                        </p>
+						<figure class="pull-centre">
+							<img src="./img/BLOGS/3.png" alt="">
+						</figure>
+
+						<h4> Step 4:</h4>In the SQL field, you can specify the queries for your database table.
+                        </p>
+						<figure class="pull-centre">
+							<img src="./img/BLOGS/4.png" alt="">
+						</figure>
+
+						
+						<p><h4>This  will help you create the desired table to store information in your database.</p></h4>
+						
+						<blockquote class="blockquote">
+							<p>The database management system (DBMS) is the software that interacts with end users, applications, and the database itself to capture and analyze the data. The DBMS software additionally encompasses the core facilities provided to administer the database.</p>
+								</blockquote>
+						<p></p>
+						<figure>
+						</figure>
+							</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+					<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Database</a></li>
+								<li><a href="#">myphpadmin</a></li>
+								<li><a href="#">tables</a></li>
+								<li><a href="#">Query</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">Avni bhardwaj</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/avatar-1.jpg" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="https://www.youtube.com/embed/-_FIgGUzz88"><img src="./img/BLOGS/5.png" alt=""></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="category.html">Technology</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="marketing-socialmediamarketing-brandawareness.php">XAMPP Installation</a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Pankaj M Patidar</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Social Media</h2>
+						</div>
+						<div class="social-widget">
+							<ul>
+								<li>
+									<a href="#" class="social-facebook">
+										<i class="fa fa-facebook"></i>
+										<span>21.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-twitter">
+										<i class="fa fa-twitter"></i>
+										<span>10.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-google-plus">
+										<i class="fa fa-google-plus"></i>
+										<span>5K<br>Followers</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- /social widget -->
+
+					<!-- category widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Categories</h2>
+						</div>
+						<div class="category-widget">
+							<ul>
+							<li><a href="#">Technology<span><!--451 --></span></a></li>
+								<li><a href="#">Fashion <span><!--230  --></span></a></li>
+								<li><a href="#">Technology <span><!-- 40 --></span></a></li>
+								<li><a href="#">Travel <span><!--38--></span></a></li>
+								<li><a href="#">Health <span><!--24--></span></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Instagram</h2>
+						</div>
+						<div class="galery-widget">
+							<ul>
+								<li><a href="#"><img src="./img/galery-1.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-2.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-3.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-4.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-5.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-6.jpg" alt=""></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
