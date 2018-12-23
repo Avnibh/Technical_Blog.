8e3e69f3025b77c2f3c5a7cbcc8f8ad304393882
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-23 21:52:13 +0530

    Add files via upload

---
+++ billionaires-tony.php
@@ -0,0 +1,280 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>Tony Toutouni is a billionaire or not ?</h3>
+						<p>I was scrolling my instagram and saw Dan Bilzerian i was thinking is he a billionaire? then there was one name who was overcoming Dan Bilzerian Who was he ? Is he a billionaire or millionaire? I was confused about it. His name was Tony Toutouni. Who is also known as “KING OF INSTAGRAM”.
+
+Tony Toutouni, is a billionaire businessman threatening to steal Dan Bilzerian's title as the King of Instagram..His outrageous posts centers around women, cars, parties and money..In just 8 months of joining Instagram, he has over 250,000 followers.
+The 42 year old told UK Mirror.
+
+What man in this world can't say this isn't the lifestyle they want to live? It's a lifestyle that every man wants to live and I enjoy living it."As a single guy, I dated many, many women. Being young in LA and having anything you want, it's not that hard to get any girl you want."I don't want to compete with Dan but people are saying that I've grown dramatically and extremely fast I can tell you that," 
+ "But Dan is actually a friend of mine and the whole Instagram thing started because he was posting on it. I saw that his Instagram following was growing pretty fast so I started doing it. The more I posted, the more people would tag and tag."
+</p>
+						<figure class="pull-right">
+							<img src="./img/BLOGS/tony.jpg" alt="billionaire tony">
+							<figcaption></figcaption>
+						</figure>
+						<p>Businesses can use Twitter to interact with prospective clients, answer questions, release latest news and at the same time use the targeted ads with specific audiences. 
+YouTube is the largest and most popular video-based social media website. 
+While Instagram is a visual social media platform. Many of its users use it to post information about travel, fashion, food, art and similar subjects. The platform is also distinguished by its unique filters together with video and photo editing features. 
+WhatsApp Messenger is an instant messaging client for smartphones, PCs and tablets. The app relies on the Internet to send images, texts, documents, audio and video messages to other users that have the app installed on their devices.
+ 
+
+  </p>
+								</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+				<!--	<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Digital Marketing</a></li>
+								<li><a href="#">Social</a></li>
+								<li><a href="#">Media</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">SAKSHAM SHARMA</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/avatar-2.jpg" alt="saksham_fuerte">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>MARKETING HEAD</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="billionaire-dan.php"><img src="./img/BLOGS/dan.jpeg" alt="brand_awareness"></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="billionaire-dan.php">Billionaires</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="billionaire-dan.php">DAN Bilzerian: Billionaire?
+</a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Saksham Sharma</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="social-google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+              <!-- /social widget -->
+
+					<!-- category widget -->
+					<?php
+					include "cate.php"
+					?>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Instagram</h2>
+						</div>
+						<div class="galery-widget">
+							<ul>
+								<li><a href="#"><img src="./img/galery-1.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-2.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-3.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-4.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-5.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-6.jpg" alt=""></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
