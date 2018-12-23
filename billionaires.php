5ea6fcd81801819a100b92751782dc8c1c0dc4ed
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-23 21:51:45 +0530

    Add files via upload

---
+++ billionaires.php
@@ -0,0 +1,157 @@
+<?php
+include "header.php";
+?>
+
+<body>
+
+<!-- SECTION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <div class="col-md-8">
+                <!-- row -->
+                <div class="row">
+               <!--     <div class="col-md-12">
+                        <div class="section-title">
+                            <h2 class="title">Recent posts</h2>
+                        </div>
+                    </div>
+                    <!-- post -->
+                    <div class="col-md-6">
+                        <div class="post">
+                            <a class="post-img" href="billionaires-dan.php"><img src="./img/BLOGS/dan.jpeg" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                <a href="billionaires-dan.php">Billionaires</a>
+                             
+                                </div>
+                                <h3 class="post-title"><a href="billionaires-dan.php">DAN Bilzerian is a billionaire?</a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="author.html">Saksham Sharma</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                   
+                    <div class="col-md-6">
+                        <div class="post">
+                            <a class="post-img" href="billionaires-tony.php"><img src="./img/BLOGS/tony.jpg" alt=""></a>
+                            <div class="post-body">
+                                <div class="post-category">
+                                <a href="billionaires-tony.php">Billionaires</a>
+                             
+                                </div>
+                                <h3 class="post-title"><a href="billionaires-tony.php">Tony Toutouni : Billionaire ?</a></h3>
+                                <ul class="post-meta">
+                                    <li><a href="author.html">Saksham Sharma</a></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- /post -->
+
+                    <!-- post -->
+                    
+                   
+                    <div class="clearfix visible-md visible-lg"></div>
+
+                    <!-- /post -->
+                </div>
+                <!-- /row -->
+
+          
+             
+                    <!--
+                    include "Fashion&Travel.php";
+                     include "tutorials.php";
+           
+             include "Health.php";
+           -->
+            
+            </div>
+            <div class="col-md-4">
+                <!-- ad widget-->
+                <div class="aside-widget text-center">
+                    <a href="#" style="display: inline-block;margin: auto;">
+                      <!--  <img class="img-responsive" src="./img/ad-3.jpg" alt=""> -->
+                    </a>
+                </div>
+                
+                <!-- /ad widget -->
+
+                <!-- social widget -->
+                <div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="social-google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /social widget -->
+
+            <?php
+            include "cate.php";
+            ?>
+
+
+               <?php
+              ?>
+            </div>
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /SECTION -->
+
+<!-- SECTION -->
+<div class="section">
+    <!-- container -->
+    <div class="container">
+        <!-- row -->
+        <div class="row">
+            <!-- ad -->
+            <div class="col-md-12 section-row text-center">
+                <a href="#" style="display: inline-block;margin: auto;">
+                 <!--   <img class="img-responsive" src="./img/ad-2.jpg" alt=""> -->
+                </a>
+            </div>
+            <!-- /ad -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /container -->
+</div>
+<!-- /SECTION -->
+
+
+</body>
+
+<?php
+include "footer.php";
+?>
