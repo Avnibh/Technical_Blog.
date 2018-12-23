4bfa469cbc1085016226d962c666582c19ec098e
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-23 21:52:00 +0530

    Add files via upload

---
+++ billionaires-dan.php
@@ -0,0 +1,265 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>DAN Bilzerian is a billionaire?
+</h3>
+						<p>Dan Bilzerian is how 13-year-old boys might imagine adulthood. Dan is an all-American pro poker player who became famous on Instagram for being both exceptionally rich and bearded, and spending his entire life shooting guns, partying, flying in private jets and having constant, uncomplicated sex with actual piles of women.
+But Dan Bilzerian isn't real in the traditional sense, he's more like an advertisement for bachelorhood. Dan is a poster boy for what life could be if us guys stopped being afraid and started working out. In the real world there are problems like bankruptcy, chlamydia, and self-reflection, but Dan doesn't live in the real world. </p>
+
+<p>
+<figure class="pull-right">
+							<img src="./img/BLOGS/dan.jpeg" alt="dan_billionaire">
+							<figcaption></figcaption>
+						</figure>
+											Instead, Dan has 16.9 million followers who observe his hyper-existence through their phones and feel mesmerised and a little bit sad. Because in truth, most of us just aren't like that.
+I feel sad. In terms of lifestyle heroes for single white guys, Dan Bilzerian is pretty much all we have. When I get home alone at night and house is dark and the cat has done a fucking number on the kitty tray, there's no male version of Bridget Jones or Carrie Bradshaw to make me think Oh well, ho hum, at least I have my friends and a box of Whitman's Sampler. Instead there's just Dan Bilzerian and his blockbuster life. And although he's probably an asshole and his photos are all pretend, I still end up thinking if only I could live like that—I'd be happy.</p>
+
+
+
+							</p>
+
+							</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+				<!--	<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Social</a></li>
+								<li><a href="#">Lifestyle</a></li>
+								<li><a href="#">Fashion</a></li>
+								<li><a href="#">Health</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">Saksham Sharma</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/avatar-2.jpg" alt="saksham_fuerte">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>Marketing Head.</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-5">
+								<div class="post post-sm">
+									<a class="post-img" href="billionaires-tony.php"><img src="./img/BLOGS/tony.jpg" alt=""></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="billionaires-tony.php">Billionaires</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="billionaires-tony.php">Tony Toutouni : Billionaire ?</a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">SAKSHAM SHARMA</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="social-google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+              	<!-- /social widget -->
+
+					<!-- category widget -->
+					<?php
+					include "cate.php"
+					?>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<?php
+					include "instagram.php"
+					?>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
